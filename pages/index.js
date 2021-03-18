4c0f0ea976e0874283cafbbae533660c9dff8e6c
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2021-03-18 14:52:30 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -20,30 +20,32 @@ export default function Home() {
           <SectionTitle addLine={true}>A little about me</SectionTitle>
           <p className="my-14 text-center mx-auto" style={{ maxWidth: "640px" }}>
             Single threaded, high performance.<br />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor id in id molestie vitae amet pellentesque. Potenti nulla sapien ullamcorper gravida sed in dolor massa. Et posuere sollicitudin turpis ac lacus. Neque mi commodo arcu risus, leo tortor risus.
+            Very focused and motivated, with high attention to details. Not being able to efficiently multitask is my weakness and it only goes to prove that I'm only human afterall.
+            I like to work on cool projects that brings out every ounce of best in me.<br/>
+            The discovery is my "reward".
           </p>
           <SmallTitle addLine={true}>
             Explore my environments
           </SmallTitle>
           <div className="flex flex-grow items-stretch space-x-16 mt-14">
             <Card title="Social Me">
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://www.linkedin.com/in/frank-choongsaeng-9a6725176/">
                 <img src="/images/linkedin logo.png" alt="linkedin logo" className="h-6" />
                 <p>linkedin</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="mailto:frankchoongsaeng@gmail.com">
                 <img src="/images/gmail logo.png" alt="gmail logo" className="h-6" />
                 <p>email</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://twitter.com/choongsaengF">
                 <img src="/images/twitter logo.png" alt="twitter logo" className="h-6" />
                 <p>twitter</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://www.facebook.com/frank.ebuka.94/">
                 <img src="/images/facebook logo.png" alt="facebook logo" className="h-6" />
                 <p>facebook</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://www.instagram.com/frank_choongsaeng/">
                 <img src="/images/instagram logo.png" alt="instagram logo" className="h-6" />
                 <p>instagram</p>
               </ListItem>
@@ -52,15 +54,15 @@ export default function Home() {
             <span className="block border-l border-black transform translate-y-2/4 opacity-20"></span>
 
             <Card title="Repos & Playgrounds">
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://github.com/frankchoongsaeng">
                 <img src="/images/github logo.png" alt="github logo" className="h-6" />
                 <p>github</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://codepen.io/frankchoongsaeng/">
                 <img src="/images/codepen logo.png" alt="codepen logo" className="h-6" />
                 <p>codepen</p>
               </ListItem>
-              <ListItem className="mr-4" link="https://linkedin.com">
+              <ListItem className="mr-4" link="https://codesandbox.io/u/frankchoongsaeng">
                 <img src="/images/codesandbox logo.png" alt="codesandbox logo" className="h-6" />
                 <p>codesandbox</p>
               </ListItem>
@@ -138,6 +140,10 @@ export default function Home() {
               <img src="/images/linkedin logo.png" alt="linkedin logo" className="h-8" />
               <ItemProficiency item="Wordpress" proficiency="70" />
             </ListItem>
+            <ListItem className="mr-4 font-medium text-sm items-center">
+              <img src="/images/linkedin logo.png" alt="linkedin logo" className="h-8" />
+              <ItemProficiency item="Python" proficiency="70" />
+            </ListItem>
           </div>
 
           <SmallTitle addLine={true}>
@@ -176,17 +182,17 @@ export default function Home() {
               className="flex flex-wrap items-stretch space-x-4 mx-auto text-sm"
             >
               <div className="flex-1 border-4 border-gray-500 overflow-hidden rounded">
-                <textarea className="h-full w-full resize-none p-3 focus:opacity-100 focus:outline-none opacity-60" name="message" id="message" placeholder="Type your message here..."></textarea>
+                <textarea className="transition-all h-full w-full resize-none p-3 focus:opacity-100 outline-none focus:outline-none opacity-60" name="message" id="message" placeholder="Type your message here..."></textarea>
               </div>
               <div className="flex flex-col items-stretch space-y-4 flex-1">
                 <div className="ct-form-control">
-                  <input className="w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 focus:outline-none opacity-60" type="text" placeholder="Full Name" />
+                  <input className="transition-all w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 outline-none focus:outline-none opacity-60" type="text" placeholder="Full Name" />
                 </div>
                 <div className="ct-form-control">
-                  <input className="w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 focus:outline-none opacity-60" type="text" placeholder="Email" />
+                  <input className="transition-all w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 outline-none focus:outline-none opacity-60" type="text" placeholder="Email" />
                 </div>
                 <div className="ct-form-control">
-                  <input className="w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 focus:outline-none opacity-60" type="text" placeholder="Subject" />
+                  <input className="transition-all w-full p-3 border-4 border-gray-500 rounded-md focus:opacity-100 outline-none focus:outline-none opacity-60" type="text" placeholder="Subject" />
                 </div>
                 <div className="self-start">
                   <Button className="align" variant="dark">Send Message</Button>
