449a21eade9292acee9818755e9b473fa1e249c5
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2023-06-29 19:16:48 +0000

    configured private mail service

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,47 +1,53 @@
-import nodemailer from 'nodemailer';
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
 
-export default (req, res) => {
-    const { fullname, email, message, subject } = req.body || {};
+interface ReqBody {
+  fullname: string;
+  email: string;
+  message: string;
+  subject: string;
+}
 
-    if (!fullname || !email || !message || !subject) {
-        return res.status(400).json({ error: true, message: 'missing or invalid parameters' });
-    }
+// create a nodemailer transporter
+let transporter = nodemailer.createTransport({
+  host: "mail.privateemail.com",
+  port: 465,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
-    // create a nodemailer transporter
-    let transporter = nodemailer.createTransport({
-        port: 465,
-        host: 'smtp.gmail.com',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-        secure: true,
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { fullname, email, message, subject }: ReqBody = req.body || {};
+
+  if (!fullname || !email || !message || !subject) {
+    return res
+      .status(400)
+      .json({ error: true, message: "missing or invalid parameters" });
+  }
+
+  console.log(process.env.EMAIL, process.env.EMAIL_PASSWORD);
+
+  try {
+    // send the mail
+    await transporter.sendMail({
+      from: "site@frankchoongsaeng.com",
+      to: process.env.EMAIL,
+      replyTo: email,
+      subject,
+      text: message,
+      date: new Date().toUTCString(),
     });
-
-    console.log(process.env.EMAIL, process.env.PASSWORD);
-
-    transporter.sendMail(
-        {
-            from: 'franchoongsaeng@gmail.com',
-            to: process.env.EMAIL,
-            replyTo: email,
-            subject,
-            text: message,
-            date: new Date().toUTCString(),
-            headers: {
-                priority: 'high',
-            },
-        },
-        err => {
-            if (err) {
-                console.log(err);
-                return res.status(450).json({
-                    error: true,
-                    message:
-                        'Requested mail action not taken: mailbox unavailable (e.g., mailbox busy or temporarily blocked for policy reasons)',
-                });
-            }
-            res.status(200).json({ error: false, message: 'sent' });
-        }
-    );
+    console.log("done");
+    res.status(200).json({ error: false, message: "sent" });
+  } catch (error) {
+    // log the error if any
+    console.log(error);
+    res.status(450).json({
+      error: true,
+      message:
+        "Requested mail action not taken: mailbox unavailable (e.g., mailbox busy or temporarily blocked for policy reasons)",
+    });
+  }
 };
